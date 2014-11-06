191dccb20cda30f7b73c1641c13c29afbf93a6fa
Emelia80 <jenny.emelia.nikolaisen@hotmail.com>
2014-11-06 18:25:27 +0100

    Oppgave 2 i kap.7
    Funker ikke heeeelt som det skal!

---
+++ Kap7/Oppgave2_kap7.java
@@ -0,0 +1,41 @@
+import javax.swing.JOptionPane;
+import javax.swing.JTextArea;
+
+public class Oppgave2_kap7
+{
+	public static void main( String args[] )
+	{
+		int [] liste = new int [10];
+		int input = 0;
+
+		JTextArea tekstområde = new JTextArea();
+		tekstområde.setText("Tall vist i stjerner\n");
+
+		for (int i=0; i<liste.length; i++)
+				{
+				input = Integer.parseInt(JOptionPane.showInputDialog("Skriv inn et heltall."));
+				liste [i] = input;
+
+				if (liste[i] >0 && liste[i] <51)
+				{
+					liste[i] = input;
+
+					for (int a=0; a<10; a++)
+					{
+						if (liste [i] !=0)
+						{
+							for (int j=1; j<liste[j]+1; j++)
+							tekstområde.append("*");
+						}
+						tekstområde.append("\n");
+					}
+				}
+				else
+				{
+					liste[i] = 0;
+					tekstområde.append("\n");
+				}
+				}
+				JOptionPane.showMessageDialog(null, tekstområde);
+	}
+}
